9b6923f719bc183363e1faa23c5db06a5c9dd2b0
brantje <brantje@gmail.com>
2022-11-18 12:56:22 +0100

    Fixes

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/emudeck-compatibility-list/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/emudeck-compatibility-list/" : "/",
+  
+  // Configure webpack is required for developing on WSL
+  configureWebpack: {
+    devServer: {
+      watchOptions: {
+        poll: true
+      }
+    }
+  }
 };
